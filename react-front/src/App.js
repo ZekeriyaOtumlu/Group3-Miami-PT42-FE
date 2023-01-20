426a2bd889881721496538d47c870ee4e312d692
dsmith225 <douglaseugenesmithjr@gmail.com>
2023-01-20 01:20:05 +0000

    added nav bar with routes

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import NavbarComponent from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Pricing } from './components/Pricing';
+import { Recipes } from './components/Recipes';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <header className="App-header">
         <NavbarComponent/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/pricing' element={<Pricing/>}/>
+          <Route path='/recipes' element={<Recipes/>}/>
+        </Routes>
+        </BrowserRouter>
+        </div>
   );
 }
 
